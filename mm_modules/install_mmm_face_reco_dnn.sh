2dcde0b9a5d4a46fafada535172b5e1c287fed10
Ismar Slomic <ismar.slomic@ruter.no>
2021-03-24 00:27:31 +0100

    replace with fork ismarslomic/MMM-Face-Reco-DNN.git and split the install script in two files

--- mm_modules/install_mmm_face_reco_dnn.sh
+++ mm_modules/install_mmm_face_reco_dnn.sh
@@ -9,59 +9,12 @@ source "$DIR/../log_util/log.sh"
 
 trap e_onScriptFailed 1 2 3 15 ERR
 
-function installOpenCV() {
-  # see https://github.com/cyysky/OpenCV-Raspberry-Pi-4-Package-for-Python
-
-  target="/home/pi/opencv"
-  mkdir -p $target
-  wget https://github.com/cyysky/OpenCV-Raspberry-Pi-4-Package-for-Python/raw/master/opencv_4.5.0-1_armhf.deb -P $target
-  sudo dpkg -i $target/opencv_4.5.0-1_armhf.deb || true # This will fail because of missing deps, using || true to continue
-  sudo apt-get -f install -y                            # Auto install dependency package
-  sudo dpkg -i $target/opencv_4.5.0-1_armhf.deb         # Now start install
-  export LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libatomic.so.1.2.0
-
-  e_success "Installing OpenCV completed"
-}
-
-function installDlib() {
-  pip3 install dlib
-
-  e_success "Installing dlib lib completed"
-}
-
-function installFaceRecognition() {
-  pip3 install face_recognition
-
-  e_success "Installing face_recognition lib completed"
-}
-
-function installImutils() {
-  pip3 install imutils
-
-  e_success "Installing imutils lib completed"
-}
-
-function installNumpy() {
-  pip3 install numpy
-
-  e_success "Installing numpy lib completed"
-}
-
-function installMMMFaceRecoDNN() {
-  git clone https://github.com/nischi/MMM-Face-Reco-DNN.git ~/MagicMirror/modules/MMM-Face-Reco-DNN
-  (cd ~/MagicMirror/modules/MMM-Face-Reco-DNN && npm install)
-}
-
 # see https://github.com/nischi/MMM-Face-Reco-DNN
 function main() {
   e_header "Installing Magic Mirror module: MMM-Face-Reco-DNN"
 
-  installOpenCV
-  installDlib
-  installFaceRecognition
-  installImutils
-  installNumpy
-  installMMMFaceRecoDNN
+  git clone https://github.com/ismarslomic/MMM-Face-Reco-DNN.git ~/MagicMirror/modules/MMM-Face-Reco-DNN
+  (cd ~/MagicMirror/modules/MMM-Face-Reco-DNN && npm install)
 
   e_success "Installing MMM-Face-Reco-DNN module completed"
 }
