0d4de461b404b2e70f88ad0245db145a0e385c6d
Ismar Slomic <ismar@slomic.no>
2023-04-29 00:49:02 +0200

    docs(monitor): adjust comments in ha scripts for monitor brightness

--- ha_scripts/smartmirror_monitor_brightness_day.sh
+++ ha_scripts/smartmirror_monitor_brightness_day.sh
@@ -2,8 +2,7 @@
 
 # Setting brightness and contrast of monitor by using ddccontrol command (https://github.com/ddccontrol/ddccontrol)
 function main() {
-  # Set brightness (0x10) to 75% at day
-  # Set contrast (0x12) to 100% at day
+  # Set brightness (0x10) to 75% and contrast (0x12) to 100% at day time
   # You might need to modify the monitor id (/dev/i2c-20) to your monitor id
   ddccontrol -r 0x10 -w 75 dev:/dev/i2c-20 && ddccontrol -r 0x12 -w 100 dev:/dev/i2c-20
 }
--- ha_scripts/smartmirror_monitor_brightness_night.sh
+++ ha_scripts/smartmirror_monitor_brightness_night.sh
@@ -2,10 +2,9 @@
 
 # Setting brightness and contrast of monitor by using ddccontrol command (https://github.com/ddccontrol/ddccontrol)
 function main() {
-  # Set brightness (0x10) to 10% at night
-  # Set contrast (0x12) to 10% at night
+  # Set brightness (0x10) to 10% and contrast (0x12) to 10% at night time
   # You might need to modify the monitor id (/dev/i2c-20) to your monitor id
-  dccontrol -r 0x10 -w 10 dev:/dev/i2c-20 && ddccontrol -r 0x12 -w 10 dev:/dev/i2c-20
+  ddccontrol -r 0x10 -w 10 dev:/dev/i2c-20 && ddccontrol -r 0x12 -w 10 dev:/dev/i2c-20
 }
 
 main
